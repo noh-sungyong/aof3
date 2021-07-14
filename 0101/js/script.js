e10687d4dcf74bffef27959d28ba6695adae883a
노성용 <sungyong2@4csoft.com>
2021-07-14 19:18:00 +0900

    다중 학습 기능 구현

---
+++ 0101/js/script.js
@@ -0,0 +1,20 @@
+/**
+	2019. 03. 03
+	FIT FRAME
+	funnysky
+	funnysky@fitframe.co.kr	
+**/
+
+/**
+	script 자막
+**/
+
+var scriptInfo = new Array(curTol);
+scriptInfo[1] = "";
+scriptInfo[2] = "학습을 시작하기 전 여러분의 사전지식을 테스트해 보겠습니다. 제시되는 질문에 O, X로 답해주세요.";
+scriptInfo[3] = "오늘의 이슈와 학습목표를 확인해봅시다.";
+scriptInfo[4] = "";
+scriptInfo[5] = "";
+scriptInfo[6] = "준비가 되셨으면 START 버튼을 클릭하여 퀴즈를 풀어보세요.";
+scriptInfo[7] = "이번 시간에 배운 핵심 내용을 정리해 봅시다.";
+scriptInfo[8] = "이것으로 오늘의 학습을 마치겠습니다. 수고하셨습니다.";
